5e4d8213ff64ed74ab9c638da4d2f4d4b978782e
kambham vivek shankar <v_706@yahoo.com>
2020-10-13 09:27:06 +0530

    Refactoring config

--- src/main/java/com/reputation/mutualfundreturns_kambhamvivek/config/AppConfig.java
+++ src/main/java/com/reputation/mutualfundreturns_kambhamvivek/config/AppConfig.java
@@ -3,13 +3,15 @@ package com.reputation.mutualfundreturns_kambhamvivek.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
-@ComponentScan("com.reputation.mutualfundreturns_kambhamvivek")
 public class AppConfig {
 
     @Bean
@@ -21,5 +23,12 @@ public class AppConfig {
         converter.setObjectMapper(mapper);
         restTemplate.getMessageConverters().add(converter);
         return restTemplate;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
     }
 }
